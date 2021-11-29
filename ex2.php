296196b33a753b6bec25dda2601db6449247d753
phn921 <panghenam@gmail.com>
2021-11-29 19:30:01 +0800

    function basic

---
+++ ex2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>function</title>
+</head>
+<body>
+    <h1>function</h1>
+    <h2>basic</h2>
+    <?php
+     function basic(){
+         print("Lorem ipsum dolor1<br>");
+         print("Lorem ipsum dolor2<br>");
+     }
+         basic();
+    ?>
+     
+</body>
+</html>
